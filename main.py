25bfa3475efb431f0ef865ac70d003bb5eac1058
cybercielo <cielo.i@northeastern.edu>
2026-01-12 14:22:57 -0500

    hi

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 """
 TODO: A very useful temperature-conversion app.
 """
+def is_it_cold_f(temp_f: float) -> bool:
+    """
+    Determines if the supplied temp is below a threshold
+
+    Parameters
+    ==========
+    temp_f : float
+        supplied temp in F
+
+    Returns
+    =======
+    bool
+        True if below threshold
+    """
+    if temp_f < 68:
+        return True 
+    else:
+        return False
+
+def greet_human() -> None:
+    """
+    Get a name from the keyboard and say hello!
+    """
+    name: str = input("what is your name?")
+
+    print(f"hello {name}")
 
 def main() -> None:
-    print("hello, world!")
-    name: str = input('what is your name? ')
-
-    print(f"hello { name }")
+    pass
 
 if __name__ == "__main__":
     main()
--- tests.py
+++ tests.py
@@ -1,3 +1,45 @@
 import unittest
+from unittest.mock import patch, Mock
 
-import main
+from main import )
+    greet_human,
+)
+
+class TestMainApp(unittest, TestCase):
+    def test_isitcoldf_freezing(self) -> None:
+        """Test is_it_cold_f for freezing"""
+        self.assertTrue(is_it_cold_f(32))
+
+    def test_isitcoldf_boiling(self) -> None:
+        """Test is_it_cold_f for boiling"""
+        self.assertFalse(is_it_cold_f(212))
+
+    def test_isitcoldf_negative(self) -> None:
+        """Test is_it_cold_f for neg temps"""
+        self.assertTrue(is_it_cold_f(-40))
+
+    def test_isitcoldf_thresh(self) -> None:
+        """Test is_it_cold_f for threshold"""
+        self.assertFalse(is_it_cold_f(68))
+
+    def test_isitcoldf_above(self) -> None:
+        """Test is_it_cold_f for above the threshold"""
+        self.assertFalse(is_it_cold_f(70))
+
+    def test_isitcoldf_below(self) -> None:
+        """Test is_it_cold_f for below the threshold"""
+        self.assertTrue(is_it_cold_f(67))
+
+
+    @patch('builtins.input', side_effect=['alice'])
+    @patch('builtins.print')
+    def test_greethuman_alice(self, mock_print: Mock, _: Mock) -> None:
+        """Test imputting alice to greet_human"""
+        greet_human()
+        expected_calls = [
+            unittest.mock.call("hello alice!"),
+        ]
+        mock_print.assert_has_calls(expected_calls)
+
+if __name__ == "__main__":
+    unittest.main()
